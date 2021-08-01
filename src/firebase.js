83d9bd658ea98ac13a729f542211df00c2e5aa06
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-08-01 20:09:32 +0900

    firebaseの設定

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const firebaseConfig={
+var firebaseConfig={
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
