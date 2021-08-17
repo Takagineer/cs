6ec19391a96453e2dc12d9610addce89bc25c210
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-08-17 17:27:27 +0900

    swiperの実装

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,8 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
 import BusinessCard from "./BusinessCard";
+import { Swiper, SwiperSlide, Autoplay } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
 
 export default function Content() {
   const cardContents = [
@@ -23,29 +25,35 @@ export default function Content() {
   return (
     <>
       <Grid container spacing={2}>
-        <Grid item xs={10}>
-          <BusinessCard
-            companyName={cardContents[0].companyName}
-            image={"https://picsum.photos/150"}
-            text={"詳細説明"}
-          />
-        </Grid>
-
-        <Grid item xs={10}>
-          <BusinessCard
-            companyName={cardContents[1].companyName}
-            image={"https://picsum.photos/200"}
-            text={"詳細説明"}
-          />
-        </Grid>
-
-        <Grid item xs={10}>
-          <BusinessCard
-            companyName={cardContents[2].companyName}
-            image={"https://picsum.photos/300"}
-            text={"詳細説明"}
-          />
-        </Grid>
+        <Swiper slidesPerView={1}>
+          <SwiperSlide>
+            <Grid item xs={10}>
+              <BusinessCard
+                companyName={cardContents[0].companyName}
+                image={"https://picsum.photos/150"}
+                text={"詳細説明"}
+              />
+            </Grid>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Grid item xs={10}>
+              <BusinessCard
+                companyName={cardContents[1].companyName}
+                image={"https://picsum.photos/200"}
+                text={"詳細説明"}
+              />
+            </Grid>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Grid item xs={10}>
+              <BusinessCard
+                companyName={cardContents[2].companyName}
+                image={"https://picsum.photos/300"}
+                text={"詳細説明"}
+              />
+            </Grid>
+          </SwiperSlide>
+        </Swiper>
       </Grid>
 
       <br />
