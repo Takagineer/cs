b7931b45537d4ff3592aa7c0880a5a88a830bdc2
Takagineer <itengineer0415@gmail.com>
2021-08-24 10:02:30 +0900

    保存機能の作成及び確認

--- src/pages/individual-pages/CompanyBusinesses.jsx
+++ src/pages/individual-pages/CompanyBusinesses.jsx
@@ -1,6 +1,6 @@
-import { makeStyles, TextField } from "@material-ui/core";
+import { Button, makeStyles, TextField } from "@material-ui/core";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import App from "../../components/App";
 import { db } from "../../firebase";
 
@@ -14,41 +14,136 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function CompanyBusinesses() {
+  const [business, setBusiness] = useState("");
+  const [detail, setDetail] = useState("");
+  const [reward, setReward] = useState("");
+  const [number, setNumber] = useState("");
+  const [location, setLocation] = useState("");
+  const [skill, setSkill] = useState("");
+  const [message, setMessage] = useState("");
+
   const addBusinessData = async () => {
+    // documentの取得;
     console.log("保存します");
-    await db.collection("Businesses").doc("1").set({ name: "test", age: 10 });
+    await db.collection("Businesses").add({
+      business: business,
+      detail: detail,
+      reward: reward,
+      number: number,
+      location: location,
+      skill: skill,
+      message: message,
+    });
     console.log("保存しました");
+    setBusiness("");
+    setDetail("");
+    setReward("");
+    setNumber("");
+    setLocation("");
+    setSkill("");
+    setMessage("");
+    //コレクションに紐づいたデータの取得
+    // const snapshot = await db
+    //   .collection("Businesses")
+    //   .where("age", "<=", 30)
+    //   .get();
+    // snapshot.forEach((doc) => {
+    //   console.log(doc.id, "=>", doc.data());
+    // });
   };
 
+  const businessValue = (e) => {
+    setBusiness(e.target.value);
+  };
+  const detailValue = (e) => {
+    setDetail(e.target.value);
+  };
+  const rewardValue = (e) => {
+    setReward(e.target.value);
+  };
+  const numberValue = (e) => {
+    setNumber(e.target.value);
+  };
+  const locationValue = (e) => {
+    setLocation(e.target.value);
+  };
+  const skillValue = (e) => {
+    setSkill(e.target.value);
+  };
+  const messageValue = (e) => {
+    setMessage(e.target.value);
+  };
   const classes = useStyles();
   return (
     <>
       <App>
         <br />
         <br />
-        <h1>募集業務張り出し機能用のページです</h1>
+        <br />
         <form className={classes.root} noValidate autoComplete="off">
-          <TextField id="standard-basic" label="募集業務" />
+          <TextField
+            id="standard-basic"
+            label="募集業務"
+            value={business}
+            onChange={businessValue}
+          />
           <br />
-          <TextField id="standard-basic" label="業務内容" />
+          <TextField
+            id="standard-basic"
+            label="業務内容"
+            value={detail}
+            onChange={detailValue}
+          />
           <br />
-          <TextField id="standard-basic" label="想定報酬額" />
+          <TextField
+            id="standard-basic"
+            label="想定報酬額"
+            value={reward}
+            onChange={rewardValue}
+          />
           <br />
-          <TextField id="standard-basic" label="募集人数" />
+          <TextField
+            id="standard-basic"
+            label="募集人数"
+            value={number}
+            onChange={numberValue}
+          />
           <br />
-          <TextField id="standard-basic" label="勤務場所" />
+          <TextField
+            id="standard-basic"
+            label="勤務場所"
+            value={location}
+            onChange={locationValue}
+          />
           <br />
-          <TextField id="standard-basic" label="必要なスキル" />
+          <TextField
+            id="standard-basic"
+            label="必要なスキル"
+            value={skill}
+            onChange={skillValue}
+          />
           <br />
-          <TextField id="standard-basic" label="メッセージ" />
+          <TextField
+            id="standard-basic"
+            label="メッセージ"
+            value={message}
+            onChange={messageValue}
+          />
 
           <br />
         </form>
-        <button onClick={addBusinessData}>追加</button>
-        <br />
 
+        <br />
+        <br />
+        <Button variant="contained" color="primary" onClick={addBusinessData}>
+          追加
+        </Button>
+        <br />
+        <br />
         <Link href="/">
-          <a>トップへ</a>
+          <Button variant="contained" color="secondary">
+            トップページへ
+          </Button>
         </Link>
       </App>
     </>
