eccff6c30ae8a2acaeffee5aa96b5e09c37c2fca
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-08-25 15:17:14 +0900

    企業様ページからのデータの投稿、保存、トップページへの表示機能

--- src/components/RankingBusiness.jsx
+++ src/components/RankingBusiness.jsx
@@ -1,29 +1,78 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import styled from "styled-components";
+import { db } from "../firebase";
 
 export default function RankingBusiness() {
+  const [businessData, setBusinessData] = useState([]);
+
+  useEffect(() => {
+    // const getBusinessData = async () => {
+    //   const getBusinessDataCollection = await db.collection("Businesses").get();
+    //   const _businessData = [];
+    //   getBusinessDataCollection.forEach((businessData) => {
+    //     _businessData.push({
+    //       businessId: businessData.id,
+    //       ...businessData.data(),
+    //     });
+    //     setBusinessData(_businessData);
+    //   });
+    // };
+    const getBusinessData = db
+      .collection("Businesses")
+      .onSnapshot((querySnapshot) => {
+        const _businesses = querySnapshot.docs.map((doc) => {
+          return {
+            businessId: doc.id,
+            ...doc.data(),
+          };
+        });
+        setBusinessData(_businesses);
+      });
+  }, []);
+
   return (
     <>
       <COntainer>
-        <p>保有しているステータスによって振り分け流機能。ボタンを押すと表示内容が変わる。</p>
-        <div>いいね数の数に応じてランキングを作成する</div>
-        <table>
-          <tr>
-            <thead>順位</thead>
-            <thead>いいね数</thead>
-            <thead>いいね数</thead>
-          </tr>
-          <tr>
-            <tbody></tbody>
-          </tr>
-        </table>
+        <br />
+        <r />
+        <br />
+        <br />
+        <br />
+        <br />
+        <UL>
+          {businessData.map((business) => {
+            return (
+              <li key={business.businessId}>
+                {business.business}
+                <br />
+                {business.location}
+                <br />
+                {business.reward}
+                <br />
+                {"星の数"}
+                <hr />
+              </li>
+            );
+          })}
+        </UL>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </COntainer>
     </>
-  )
+  );
 }
 
 const COntainer = styled.div`
-background-color:#e4dc8a;
-height:300px;
-padding:100px 0 0 40px;
-`
+  height: 300px;
+  padding: 100px 0 0 40px;
+`;
+
+const UL = styled.ul`
+  background-color: white;
+  list-style: none;
+  border-radius: 20px;
+`;
