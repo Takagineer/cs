625b2efa1b41e29dc60d852c0e8370648a662bda
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-08-25 15:32:08 +0900

    フィルターコンポーネントの作成と記述追加

---
+++ src/components/Filetr.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Filetr() {
+  return (
+    <>
+      <div>フィルター機能の実装</div>
+    </>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,14 @@ import App from "../components/App";
 import Link from "next/link";
 import PopularBusinesses from "../components/PopularBusinesses";
 import RankingBusiness from "../components/RankingBusiness";
+import Filetr from "../components/Filetr";
 
 export default function Home() {
   return (
     // <AuthProvider>
     <App>
       <PopularBusinesses />
+      <Filetr />
       <RankingBusiness />
     </App>
     // </AuthProvider>
