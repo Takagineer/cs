27f429545a9595b19b35d76b91453f146b1bac56
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-09-08 14:38:09 +0900

    storageの追加記述

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -61,4 +62,5 @@ export const signOut = async () => {
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
+export const storage = firebase.storage();
 export default firebase;
