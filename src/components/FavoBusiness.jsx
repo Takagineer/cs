8bf293942e692cb6d125e2fbeb07f71eea12d993
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-09-10 21:16:41 +0900

    タブ内のuseStateの定義と枠組み記述

--- src/components/FavoBusiness.jsx
+++ src/components/FavoBusiness.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../firebase";
+import styled from "styled-components";
 
 export default function RankingFav() {
+  const [favoBusinessData, setFavoBusinessData] = useState([]);
+
+  const getFavoBusinessData = db
+    .collection("Businesses")
+    .onSnapshot((querySnapshot) => {
+      const _favoBusinesses = querySnapshot.docs.map((doc) => {
+        return {
+          businessId: doc.id,
+          ...doc.data(),
+        };
+      });
+      setFavoBusinessData(_favoBusinesses);
+    });
   return (
     <>
-      <h1>いいね数でのランキング</h1>
+      <COntainer>
+        <UL>
+          {favoBusinessData.map((business) => {
+            return (
+              <LI key={business.businessId}>
+                業務：{business.business}
+                <br />
+                勤務場所：{business.location}
+                <br />
+                想定報酬額：{business.reward}
+                <br />
+                いいね数：{"星の数"}
+              </LI>
+            );
+          })}
+        </UL>
+      </COntainer>
     </>
   );
 }
+const COntainer = styled.div`
+  padding: 0 0 0 20px;
+`;
+
+const UL = styled.ul`
+  list-style: none;
+`;
+
+const LI = styled.li`
+  padding: 10px 20px;
+  margin: 10px;
+  border-radius: 20px;
+  border: solid 5px #59b9c6;
+`;
--- src/components/RewardBusiness.jsx
+++ src/components/RewardBusiness.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { db } from "../firebase";
 
 export default function RankingBusinessReward() {
+  const [rewardBusinessData, setRewardBusinessData] = useState([]);
+  const getRewardBusinessData = db
+    .collection("Businesses")
+    .onSnapshot((querySnapshot) => {
+      const _rewardBusinesses = querySnapshot.docs.map((doc) => {
+        return {
+          businessId: doc.id,
+          ...doc.data(),
+        };
+      });
+      setRewardBusinessData(_rewardBusinesses);
+    });
   return (
     <>
-      <h1>報酬額でのランキング</h1>
+      <COntainer>
+        <UL>
+          {rewardBusinessData.map((business) => {
+            return (
+              <LI key={business.businessId}>
+                業務：{business.business}
+                <br />
+                勤務場所：{business.location}
+                <br />
+                想定報酬額：{business.reward}
+                <br />
+                いいね数：{"星の数"}
+              </LI>
+            );
+          })}
+        </UL>
+      </COntainer>
     </>
   );
 }
+const COntainer = styled.div`
+  padding: 0 0 0 20px;
+`;
+
+const UL = styled.ul`
+  list-style: none;
+`;
+
+const LI = styled.li`
+  padding: 10px 20px;
+  margin: 10px;
+  border-radius: 20px;
+  border: solid 5px #59b9c6;
+`;
