afc15ce5a08db227eeae437f1617ef2127e4ffa5
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-09-13 10:13:48 +0900

    日時の追加について

--- src/pages/individual-pages/CompanyBusinesses.jsx
+++ src/pages/individual-pages/CompanyBusinesses.jsx
@@ -14,9 +14,6 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
-
-console.log(auth.currentUser.uid);
-
 export default function CompanyBusinesses() {
   const [business, setBusiness] = useState("");
   const [detail, setDetail] = useState("");
@@ -57,6 +54,7 @@ export default function CompanyBusinesses() {
       .doc(auth.currentUser.uid)
       .collection("Businesses")
       .add({
+        // timestamp: db.serverTimestamp(),
         business: business,
         detail: detail,
         reward: reward,
