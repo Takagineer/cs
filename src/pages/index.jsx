d7939c7df820d11048aa89b7b2b5782fe8994d56
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-10-14 16:29:34 +0900

    recoilrootの記述場所の変更

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,33 +4,30 @@ import Head from "next/Head";
 import PopularBusinesses from "../components/PopularBusinesses";
 import Filter from "../components/Filetr";
 import RankingBusiness from "../components/NewBusiness";
-import { RecoilRoot } from "recoil";
 
 export default function Home() {
   return (
-    <RecoilRoot>
-      <App>
-        <Head>
-          <title>トップページ</title>
-        </Head>
-        <PopularBusinesses />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Filter />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </App>
-    </RecoilRoot>
+    <App>
+      <Head>
+        <title>トップページ</title>
+      </Head>
+      <PopularBusinesses />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Filter />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </App>
   );
 }
