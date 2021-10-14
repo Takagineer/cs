9fcb8946fdff0cee21bc7d30cfd151d3a5fa141c
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-10-14 14:48:26 +0900

    RecoilRootで囲う記述

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,32 +4,35 @@ import Head from "next/Head";
 import PopularBusinesses from "../components/PopularBusinesses";
 import Filter from "../components/Filetr";
 import RankingBusiness from "../components/NewBusiness";
+import { RecoilRoot } from "recoil";
 
 export default function Home() {
   return (
     // <AuthProvider>
-    <App>
-      <Head>
-        <title>トップページ</title>
-      </Head>
-      <PopularBusinesses />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Filter />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-    </App>
+    <RecoilRoot>
+      <App>
+        <Head>
+          <title>トップページ</title>
+        </Head>
+        <PopularBusinesses />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Filter />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </App>
+    </RecoilRoot>
     // </AuthProvider>
   );
 }
