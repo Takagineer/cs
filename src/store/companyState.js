23207e0d84c2e9a41d65810cb902d11ec554637b
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-10-14 14:50:00 +0900

    グローバルステートの初期値変更

--- src/store/companyState.js
+++ src/store/companyState.js
@@ -2,5 +2,5 @@ import atom from "recoil";
 
 export const companyState = atom({
   key: "companyState",
-  default: { isCompanyState: false },
+  default: { uid: "" },
 });
--- src/store/studentState.js
+++ src/store/studentState.js
@@ -2,5 +2,5 @@ import atom from "recoil";
 
 export const studentState = atom({
   key: "studentState",
-  default: { isStudentAdmin: false },
+  default: { uid: "" },
 });
