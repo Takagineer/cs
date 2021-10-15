5ef2dc7dac932b67f37537460dff8d375d96e947
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-10-15 15:25:34 +0900

    recoilStateでの記述

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,13 @@ import MenuIcon from "@material-ui/icons/Menu";
 import styled from "styled-components";
 import { auth, db } from "../firebase";
 import { useRecoilState } from "recoil";
-// import { userState } from "../store/userState";
+import { userState } from "../store/userState";
 
 export default function Header({ pathname }) {
   const [anchorElCompany, setAnchorElCompany] = useState(null);
   const [anchorElStudent, setAnchorElStudent] = useState(null);
-  const [logInUser, setLogInUser] = useState();
-  // const [logInUser, setLogInUser] = useRecoilState(userState);
+  // const [logInUser, setLogInUser] = useState();
+  const [logInUser, setLogInUser] = useRecoilState(userState);
 
   const handleClickCompany = (event) => {
     setAnchorElCompany(event.currentTarget);
