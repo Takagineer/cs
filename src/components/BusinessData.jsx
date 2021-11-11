ab0f803bc4f10c2156c8768ea638039b5c7fdca7
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-11-11 16:43:56 +0900

    ヘッダーの被り解消

--- src/components/BusinessData.jsx
+++ src/components/BusinessData.jsx
@@ -106,50 +106,49 @@ export default function BusinessData() {
 
   return (
     <>
-      <App>
-        {businessData.map((business) => {
-          return (
-            <>
-              <CArd sx={{ maxWidth: 345 }}>
-                <Link
-                  href={{
-                    pathname: "individual-pages/business/[business]",
-                    query: { business: business.businessId },
-                  }}
-                >
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="300"
-                      image={business.imageURL}
-                      alt="green iguana"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {business.companyName}
-                      </Typography>
-                      <Typography gutterBottom variant="h6" component="div">
-                        {business.business}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {business.message}
-                      </Typography>
-                      <br />
-                      <Typography variant="body2" color="text.secondary">
-                        {business.location}
-                      </Typography>
-                      <br />
-                      <Typography variant="body2" color="text.secondary">
-                        {`${business.reward}/月`}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Link>
+      {businessData.map((business) => {
+        return (
+          <>
+            <CArd sx={{ maxWidth: 345 }}>
+              <Link
+                href={{
+                  pathname: "individual-pages/business/[business]",
+                  query: { business: business.businessId },
+                }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="300"
+                    image={business.imageURL}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {business.companyName}
+                    </Typography>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {business.business}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {business.message}
+                    </Typography>
+                    <br />
+                    <Typography variant="body2" color="text.secondary">
+                      {business.location}
+                    </Typography>
+                    <br />
+                    <Typography variant="body2" color="text.secondary">
+                      {`${business.reward}/月`}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Link>
 
-                {business.skill.map((skill) => {
-                  return <A key={skill.label}>{skill.label}</A>;
-                })}
-                {/* <CardActions>
+              {business.skill.map((skill) => {
+                return <A key={skill.label}>{skill.label}</A>;
+              })}
+              {/* <CardActions>
                     <br />
                     <IconButton
                       aria-label="settings"
@@ -166,12 +165,11 @@ export default function BusinessData() {
                       {isLiked === true ? "あかい" : "くろい"}
                     </IconButton>
                   </CardActions> */}
-              </CArd>
-              <br />
-            </>
-          );
-        })}
-      </App>
+            </CArd>
+            <br />
+          </>
+        );
+      })}
     </>
   );
 }
